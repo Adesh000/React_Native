226be0d120d2182995e886fc3228dc82670326c8
Adesh Nayak <adeshnayak6@gmail.com>
2022-12-16 14:17:28 +0530

    state

--- 03/StateIntro/App.js
+++ 03/StateIntro/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+        };
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    placeholder="Enter text here..."
+                    placeholderTextColor="black"
+                    style={styles.input}
+                    onChange={(text) => {
+                        this.setState({ text });
+                    }}
+                />
+                <Text style={{ marginTop: 60, fontSize: 25 }}>
+                      {this.state.text}
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    input: {
+        marginTop: 30,
+        height: 50,
+        width: 200,
+        borderWidth: 5,
+        padding: 10,
+        borderColor: "#23abab",
+    },
 });
