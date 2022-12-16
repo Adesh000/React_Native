37e715328c46be1ebc9980572e8a0804f6952eaa
Adesh Nayak <adeshnayak6@gmail.com>
2022-12-16 18:39:15 +0530

    Dice Roller App finished

---
+++ Dice_Roller_App/App.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+    const [diceOneImage, setOneDiceImage] = useState(
+        require("./src/Images/01.webp")
+    );
+    const [diceTwoImage, setTwoDiceImage] = useState(
+        require("./src/Images/01.webp")
+    );
+    const genRandomNumber = () => {
+        return Math.floor(Math.random() * 6) + 1;
+    };
+
+    const playGame = () => {
+        const rNumber = genRandomNumber();
+        const secondNumber = genRandomNumber();
+
+        switch (secondNumber) {
+            case 1:
+                setTwoDiceImage(require("./src/Images/01.webp"));
+                break;
+            case 2:
+                setTwoDiceImage(require("./src/Images/02.webp"));
+                break;
+            case 3:
+                setTwoDiceImage(require("./src/Images/03.webp"));
+                break;
+            case 4:
+                setTwoDiceImage(require("./src/Images/04.webp"));
+                break;
+            case 5:
+                setTwoDiceImage(require("./src/Images/05.webp"));
+                break;
+            case 6:
+                setTwoDiceImage(require("./src/Images/06.webp"));
+                break;
+        }
+
+        switch (rNumber) {
+            case 1:
+                setOneDiceImage(require("./src/Images/01.webp"));
+                break;
+            case 2:
+                setOneDiceImage(require("./src/Images/02.webp"));
+                break;
+            case 3:
+                setOneDiceImage(require("./src/Images/03.webp"));
+                break;
+            case 4:
+                setOneDiceImage(require("./src/Images/04.webp"));
+                break;
+            case 5:
+                setOneDiceImage(require("./src/Images/05.webp"));
+                break;
+            case 6:
+                setOneDiceImage(require("./src/Images/06.webp"));
+                break;
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={{ flexDirection: "row" }}>
+                <Image style={styles.image} source={diceOneImage} />
+                <Image style={styles.image} source={diceTwoImage} />
+            </View>
+            <TouchableOpacity onPress={playGame}>
+                <Text style={styles.gameButton}>Click Me!</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#E74292",
+    },
+    image: {
+        width: 150,
+        height: 150,
+    },
+    gameButton: {
+        fontSize: 25,
+        color: "#fff",
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+        borderColor: "#fff",
+        borderWidth: 2,
+        margin: 10,
+        borderRadius: 10,
+    },
+});
---
+++ Dice_Roller_App/src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+const Button = () => {
+    return (
+        <TouchableOpacity>
+            
+        </TouchableOpacity>
+    )
+}
+
+export default Button;
